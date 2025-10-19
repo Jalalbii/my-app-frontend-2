1c0be7c7a41aafbc7e8cad29d29bfe9d5f34506d
Jalalbii <8451031656safa@gmail.com>
2025-10-19 18:23:21 +0530

    code pushe to main

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-app-frontend/',
+  base: '/my-app-frontend-2/',
   plugins: [react()],
 })
